9ccc02585e0c665ef48d2ab865a20f8c347b3990
Toluwalase Agoro <toluwalase.agoro@stfc.ac.uk>
2021-12-29 14:46:45 +0000

    Setup initial file structure

---
+++ src/Transition_Matrix.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class EnergyLevelTransitionMatrix(object):
+
+    def __init__(self, energy_level_start, energy_level_stop):
+        pass
+
+    def read_from_file(self, path):
+        pass
+
+    def get_steady_state_population_levels(self):
+        pass
+
+    def calculate_steady_state(self):
+        pass
+
+    def get_transition_matrix(self):
+        pass
+
+    def get_energy_level_info(self):
+        pass
